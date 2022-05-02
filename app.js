e6d0b4c3cb9df787ca167ba147b64c769de592a9
jonrosario <jonrosariox@gmail.com>
2022-05-02 15:06:41 -0400

    second commit

--- app.js
+++ app.js
@@ -1,56 +1,84 @@
 let pokemon = [
   {
     'name': 'Charizard',
-    'type': 'Fire',
+    'type': 'fire',
     'image': 'Pokemon_Images/Charizard.jpg'
   },
   {
     'name': 'Blastoise',
-    'type': 'Water',
+    'type': 'water',
     'image': 'Pokemon_Images/Blastoise.jpg'
   },
   {
     'name': 'Venusaur',
-    'type': 'Grass',
+    'type': 'grass',
     'image': 'Pokemon_Images/Venusaur.jpg'
   }
 ]
 
-let container = document.querySelector('div')
-let pokeball = document.querySelector('button')
+let container = document.querySelector('.playButton')
+let pokeball = document.querySelector('#pokeball')
 
-let type1 = pokemon[0].type;
-let type2 = pokemon[1].type;
-let type3 = pokemon[2].type;
+let fire = pokemon[0].type;
+let water = pokemon[1].type;
+let grass = pokemon[2].type;
 
 
-let playerPokemon = {}
+// let playerPokemon = {}
 const choice1 = () => {
+  container.innerHTML = ''
   let newImg = document.createElement('img')
-  newImg.classList.add('player')
+  newImg.setAttribute('id', 'player')
   let randomNum = Math.floor(Math.random() * 3)
   newImg.setAttribute('src', pokemon[randomNum].image)
   container.appendChild(newImg)
-  playerPokemon = pokemon[randomNum]
-  return playerPokemon
+  return pokemon[randomNum]
 }
-pokeball.addEventListener('click', choice1)
+// pokeball.addEventListener('click', choice1)
 
-let cpuChoice = {}
+// let cpuChoice = {}
 const cpuPokemon = () => {
   let newImg2 = document.createElement('img')
-  newImg2.classList.add('cpu')
+  newImg2.setAttribute('id', 'cpu')
   let randomNum = Math.floor(Math.random() * 3)
   newImg2.setAttribute('src', pokemon[randomNum].image)
   container.appendChild(newImg2)
   cpuChoice = pokemon[randomNum]
-  if (playerPokemon.type === cpuChoice.type) {
-    console.log('draw')
-  }
-
   return pokemon[randomNum]
 }
-pokeball.addEventListener('click', cpuPokemon)
+function battle() {
+  let playerPokemon = choice1()
+  let cpuChoice = cpuPokemon()
+  console.log(playerPokemon)
+  console.log(cpuChoice)
+  let playerImg = document.getElementById('player');
+  let cpuImg = document.getElementById('cpu');
+
+  if (playerPokemon.type === cpuChoice.type) {
+    console.log('draw')
+  } else if (playerPokemon.type === fire && cpuChoice.type === grass) {
+    playerImg.style.width = '280px';
+    playerImg.style.height = '280px';
+  } else if (playerPokemon.type === water && cpuChoice.type === fire) {
+    playerImg.style.width = '200px';
+    playerImg.style.height = '200px';
+  } else if (playerPokemon.type === grass && cpuChoice.type === water) {
+    playerImg.style.width = '200px';
+    playerImg.style.height = '200px';
+  } else if (playerPokemon.type === grass && cpuChoice.type === fire) {
+    cpuImg.style.width = '280px'
+    cpuImg.style.height = '280px'
+  } else if (playerPokemon.type === fire && cpuChoice.type === water) {
+    cpuImg.style.width = '200px'
+    cpuImg.style.height = '200px'
+  } else if (playerPokemon.type === water && cpuChoice.type === grass) {
+    cpuImg.style.width = '200px'
+    cpuImg.style.height = '200px'
+  }
+}
+
+// pokeball.addEventListener('click', cpuPokemon)
+pokeball.addEventListener('click', battle)
 
 
 
